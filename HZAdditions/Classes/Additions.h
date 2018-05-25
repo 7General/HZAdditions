3e1511dc08a6fd0506ec889f070b914bde5b4179
wanghuizhou <wanghuizhou@guazi.com>
2018-05-25 17:08:07 +0800

    修改文件夹

--- HZAdditions/Classes/Additions.h
+++ HZAdditions/Classes/Additions.h
@@ -8,24 +8,35 @@
 //
 
 #import "NSString+Additions.h"
+#import "NSString+AttributedString.h"
+#import "NSString+Helper.h"
 #import "NSString+MD5.h"
 #import "NSString+NULL.h"
 #import "NSString+SEL.h"
-#import "NSString+Helper.h"
-#import "NSString+AttributedString.h"
 
+
+#import "UIImage+WaterMark.h"
 #import "UIView+Additions.h"
 #import "UIView+Appear.h"
 #import "UIView+Draw.h"
 #import "UIView+Label.h"
 #import "UIView+firstResponder.h"
 #import "UIView+RoundedCorners.h"
+#import "UIView+Label.h"
+#import "UIView+RoundedCorners.h"
+#import "UIImage+WaterMark.h"
 
+
+
+
+
+#import "HZAdditionsCont.h"
 #import "NSDate+Helper.h"
 #import "NSNumber+Additions.h"
-
-
 #import "UIButton+FillColor.h"
+
+
+
 
 
 
